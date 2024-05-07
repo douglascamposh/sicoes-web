cd5ed82276265b4851312d2c1823933e5b5570ad
douglas-campos-coderoad-com <50142435+douglas-campos-coderoad-com@users.noreply.github.com>
2024-05-07 18:07:08 -0400

    Added doccker config
    - Added CI/Cd github actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+  // Warning: This allows production builds to successfully complete even if
+  // your project has ESLint errors.
+  ignoreDuringBuilds: true,
+  },
+  output: 'standalone',
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "*" },
+          { key: "Access-Control-Allow-Private-Network", value: "true" },
+        ]
+      }
+    ]
+  },
+};
 
 export default nextConfig;
